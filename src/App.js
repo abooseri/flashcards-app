b2884297756c6300aeff083d519b0e46abe310e6
Anees Busari <aneesbusari@aneess-mbp.home>
2021-11-02 16:05:26 -0400

    Flashcards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import FlashcardList from './FlashcardList'
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+    return (
+        <div>
+            <FlashcardList flashcards={flashcards} />
+        </div>
+    )
 }
 
-export default App;
+const SAMPLE_FLASHCARDS = [
+    {
+        id: 1,
+        question: 'What is the size of the sun',
+        answer: "10000000mi",
+        options: [
+            '10000000mi',
+            '2045643453km',
+            '3425454353454mi',
+            '250 Earths'
+        ]
+    },
+    {
+        id: 2,
+        question: 'Q2',
+        answer: "Answer",
+        options: [
+            'Answer',
+            'Yess',
+            'Answer3',
+            'Answer4'
+        ]
+    },
+ ]
+
+export default App
