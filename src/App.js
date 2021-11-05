f1cf1fb01178d78b9f0db589ca15be68264c0c7b
Anees Busari <aneesbusari@aneess-mbp.home>
2021-11-05 15:25:57 -0400

    add better styles

--- src/App.js
+++ src/App.js
@@ -1,60 +1,83 @@
-import React, { useState, useEffect } from 'react'
-import FlashcardList from './FlashcardList'
+import React, { useState, useEffect, useRef } from 'react';
+import FlashcardList from './FlashcardList';
 import './app.css'
 import axios from 'axios'
 
-const App = () => {
-    const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+function App() {
+  const [flashcards, setFlashcards] = useState([])
+  const [categories, setCategories] = useState([])
 
+  const categoryEl = useRef()
+  const amountEl = useRef()
 
-    useEffect(() => {
-axios.get('https://opentdb.com/api.php?amount=10')
-        .then(res => {
-           setFlashcards(res.data.results.map((questionItem, index) => {
-               const answer = questionItem.correct_answer
-               const options = [...questionItem.incorrect_answers, answer]
-                return {
-                    id: `${index}-${Date.now()}`,
-                    question: questionItem.question,
-                    answer: answer,
-                    options: options.sort(() => Math.random() - .5)
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api_category.php')
+      .then(res => {
+        setCategories(res.data.trivia_categories)
+      })
+  }, [])
 
+  useEffect(() => {
+   
+  }, [])
 
-                }
-            }))
-        })
-    }, [])
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML= str
+    return textArea.value
+  }
 
-    return (
-        <div className="container">
-            <FlashcardList flashcards={flashcards} />
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios
+    .get('https://opentdb.com/api.php', {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)),
+          answer
+        ]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+    })
+  }
+
+  return (
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map(category => {
+              return <option value={category.id} key={category.id}>{category.name}</option>
+            })}
+          </select>
         </div>
-    )
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
+  );
 }
 
-const SAMPLE_FLASHCARDS = [
-    {
-        id: 1,
-        question: 'What is the size of the sun',
-        answer: "10000000mi",
-        options: [
-            '10000000mi',
-            '2045643453km',
-            '3425454353454mi',
-            '250 Earths'
-        ]
-    },
-    {
-        id: 2,
-        question: 'Q2',
-        answer: "Answer",
-        options: [
-            'Answer',
-            'Yess',
-            'Answer3',
-            'Answer4'
-        ]
-    },
- ]
-
-export default App
+export default App;
--- src/FlashcardList.js
+++ src/FlashcardList.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Flashcard from './Flashcard'
+import Flashcard from './Flashcard';
 
-export default function FlashcardList({ flashcards })  {
-    return (
-        <div className="card=grid">
-            {flashcards.map(flashcard => {
-                return <Flashcard flashcard={flashcard} key={flashcard.id} />
-            })}
-        </div>
-    )
+export default function FlashcardList({ flashcards }) {
+  return (
+    <div className="card-grid">
+      {flashcards.map(flashcard => {
+        return <Flashcard flashcard={flashcard} key={flashcard.id} />
+      })}
+    </div>
+  )
 }
